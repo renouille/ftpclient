6fee8c971c2937e7ad743673a655885b26ca9525
René Grossmann <rene.grossmann@iomedia.ch>
2014-06-23 09:53:58 +0200

    import java.io.file

--- src/FtpClient.java
+++ src/FtpClient.java
@@ -17,6 +17,7 @@ import android.content.Intent;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
